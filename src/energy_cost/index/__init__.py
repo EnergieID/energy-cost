97fd48aa00b257d640c2cc88c488235b09b3488a
Jorg <jorg.vanrenterghem@energieid.be>
2026-03-12 17:05:23 +0100

    Support index from files

--- src/energy_cost/index/__init__.py
+++ src/energy_cost/index/__init__.py
@@ -1,4 +1,5 @@
+from .dataframe_index import CSVIndex, DataFrameIndex, YAMLIndex
 from .entsoe_day_ahead_index import EntsoeDayAheadIndex
 from .index import Index
 
-__all__ = ["EntsoeDayAheadIndex", "Index"]
+__all__ = ["EntsoeDayAheadIndex", "Index", "DataFrameIndex", "CSVIndex", "YAMLIndex"]
